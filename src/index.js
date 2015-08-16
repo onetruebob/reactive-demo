271fb2174b96582839822cd2bfb5dc8781ed1822
Bob Owen <bob@bobowen.org>
2015-08-15 23:19:11 -0400

    Testing creating stream directly based on promise results.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 var requestStream = Rx.Observable.just('https://api.github.com/users');
+
+requestStream.subscribe(function(requestUrl){
+  var responseStream = Rx.Observable.create(function(observer) {
+    $.getJSON(requestUrl)
+      .done(function(response) { observer.onNext(response); })
+      .fail(function(jqXHR, status, error) { observer.onError(error); })
+      .always(function() { observer.onCompleted(); });
+  });
+
+  responseStream.subscribe(function(response) {
+    debugger;
+  });
+});
