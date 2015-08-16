16aee6182238ab6a2d0f20ff145d8e78c200b441
Bob Owen <bob@bobowen.org>
2015-08-16 01:14:26 -0400

    Loading now responds to clicks on refresh button.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-var requestStream = Rx.Observable.just('https://api.github.com/users');
+var refreshButton = document.querySelector('#refreshAll');
+var refreshClickStream = Rx.Observable.fromEvent(refreshButton, 'click');
+
+// var requestStream = Rx.Observable.just('https://api.github.com/users');
+var requestStream = refreshClickStream
+  .startWith('force load once')
+  .map(function() {
+    var randomOffset = Math.floor(Math.random() * 1000);
+    return 'https://api.github.com/users?since=' + randomOffset;
+  });
+
 
 requestStream.subscribe(function(requestUrl){
   var responseStream = requestStream
